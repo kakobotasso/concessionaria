ea0cd49437fca73db3b9ca7ff882f6e5d2c0566b
Kako Botasso <kakodev@gmail.com>
2017-08-30 21:16:07 -0300

    Cria models da aplicação

---
+++ src/main/java/br/com/fiap/concessionaria/model/Carro.java
@@ -0,0 +1,41 @@
+package br.com.fiap.concessionaria.model;
+
+public class Carro {
+
+    private int id;
+    private String modelo;
+    private String ano;
+    private Fabricante fabricante;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public Fabricante getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(Fabricante fabricante) {
+        this.fabricante = fabricante;
+    }
+}
